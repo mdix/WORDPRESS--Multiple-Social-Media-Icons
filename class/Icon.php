b5771d3e318f90251a46c4327ad38394a11a860a
mdix <dix@mediaman.de>
2012-03-29 13:54:27 +0200

    tests f. getMarkup() & implementation

--- class/Icon.php
+++ class/Icon.php
@@ -8,7 +8,6 @@ class Icon {
     private $name;
     private $iconSize;
     private $iconTitle;
-    private $iconAlt;
     private $iconLink;
     
     /**
@@ -30,31 +29,53 @@ class Icon {
         $this->setDefaultValues();
     }
     
+    /**
+     * Determines the left offset of the current icon in the sprite, which is the iconSize
+     * multiplied with the position of the icon (from the left), starting with 0, negated!
+     * @return int Left offset (in px) or -1 if not found
+     */
     public function getLeftOffset() {
         if (in_array($this->name, $this->availableIcons)) {
             $itemsArrayKey = array_search($this->name, $this->availableIcons);
-            return bcmul($this->iconSize, $itemsArrayKey);
+            return -bcmul($this->iconSize, $itemsArrayKey);
         }
         return -1;
     }
     
+    /**
+     * Adds a default title, alt text and link to the icon. 
+     */
     private function setDefaultValues() {
         $this->iconTitle = 'Follow me on ' . $this->name;
-        $this->iconAlt   = $this->name . ' icon';
-        $this->iconLink  = '';
+        $this->iconLink  = '#';
     }
     
+    /*
+     * Set the title tag of the icon, shown when hovering it.
+     * @param String $title The title text which will be rendered into the quotas: title=""
+     */
     public function setCustomIconTitle($title) {
         $this->iconTitle = $title;
     }
     
-    public function setCustomIconAlt($alt) {
-        $this->iconAlt = $alt;
-    }
-    
+    /**
+     * Set the link target that is called when clicking the icon.
+     * @param String $link The link target which will be rendered into the qutoas <a href=""
+     */
     public function setCustomIconLink($link) {
         $this->iconLink = $link;
     }
+    
+    /**
+     * Returns The markup of the curren icon, used to be rendered as html junk. The ###SPRITE###
+     * placeholder is replaced with the corresponding sprite image by IconSet.
+     * @return String Buttons html markup
+     */
+    public function getMarkup() {
+        $markup = '<div style="width:%1$spx;height:%1$spx;background-image:###SPRITE###;background-position:%4$spx 0;"><a style="display:block;width:%1$spx;height:%1$spx;" href="%2$s" title="%3$s" target="_blank"></a></div>';
+        return sprintf($markup, $this->iconSize, $this->iconLink, $this->iconTitle, $this->getLeftOffset());
+    }
+    
 }
 
 ?>
